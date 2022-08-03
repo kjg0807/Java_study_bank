490992fbd8b9210cf750d61d595f224cce16d20e
kjg0807 <kjg08070@naver.com>
2022-08-03 11:09:20 +0900

    TestComplete

--- src/main/java/com/naver/start/test/DBConnectionTest.java
+++ src/main/java/com/naver/start/test/DBConnectionTest.java
@@ -1,7 +1,23 @@
 package com.naver.start.test;
 
+import java.sql.Connection;
+
+import com.naver.start.util.DBConnector;
+
 public class DBConnectionTest
 {
 	// DBConnection을 호출하여 테스트
-	
+	public static void main(String[] args)
+	{
+		try
+		{
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

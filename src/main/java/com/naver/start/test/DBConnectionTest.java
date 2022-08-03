71f89ca1600267e9b3481eba837ca7be2f7e55a0
kjg0807 <kjg08070@naver.com>
2022-08-03 10:40:12 +0900

    초기화

---
+++ src/main/java/com/naver/start/test/DBConnectionTest.java
@@ -0,0 +1,7 @@
+package com.naver.start.test;
+
+public class DBConnectionTest
+{
+	// DBConnection을 호출하여 테스트
+	
+}
---
+++ src/main/java/com/naver/start/test/TestMain.java
@@ -0,0 +1,12 @@
+package com.naver.start.test;
+
+public class TestMain
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+
+	}
+
+}

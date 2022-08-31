f0dca0f9c6c5ef83b4372054a85f7ae686757821
kjg0807 <kjg08070@naver.com>
2022-08-31 09:36:09 +0900

    1

--- src/main/webapp/resources/js/dom_4.js
+++ src/main/webapp/resources/js/dom_4.js
@@ -1,5 +1,6 @@
 const ch = document.getElementsByClassName("ch");
 const cb = document.getElementsByClassName("cb");
+const all = document.getElementById("all");
 
 for (let i = 0; i < ch.length; i++) {
     ch[i].addEventListener("click", function () {
@@ -9,21 +10,50 @@ for (let i = 0; i < ch.length; i++) {
 
 // 첫번째 체크가 true면 나머지도 전부 true
 // false면 전부 false
-// for (let i = 0; i < c1.length; i++) {
-//     console.log(c1[i].selected);
+//CB
+// for (let i = 0; i < cb.length; i++) {
+//     cb[i].addEventListener("click", function () {
+//         //console.log(cb[0] == true);
+//         for (let i = 0; i < cb.length; i++) {
+//             console.log(cb[i].checked);
+//             cb[i].checked = true;
+//             // if (!cb[0].checked) {
+//             //     cb[i].checked = false;
+//             // }
+//         }
+//         console.log(cb[i].checked);
+//     })
 // }
-// c1[0].selected = true;
 
-for (let i = 0; i < cb.length; i++) {
-    cb[i].addEventListener("click", function () {
-        //console.log(cb[0] == true);
-        for (let i = 0; i < cb.length; i++) {
-            console.log(cb[i].checked);
-            cb[i].checked = true;
-            // if (!cb[0].checked) {
-            //     cb[i].checked = false;
-            // }
-        }
+// --------------------------------------------------------
+all.addEventListener("click", function () {
+    console.log(all.checked);
+    for (let i = 0; i < cb.length; i++) {
         console.log(cb[i].checked);
-    })
+        cb[i].checked = all.checked;
+    }
+});
+
+// --------------------------------------------------------
+// for (let i = 0; i < cb.length; i++) {
+//     cb[i].addEventListener("click", function () {
+//         check(1); // 매개 변수가 있는 함수는 function안에서 호출해야 함
+//     });
+// }
+for (let i = 0; i < cb.length; i++) {
+    cb[i].addEventListener("click", check);
 }
+
+function check() {
+    let rs = true;
+    for (let i = 0; i < cb.length; i++) {
+        console.log(cb[i].checked); // true
+        if (!cb[i].checked) {
+            rs = false;
+            break;
+        }
+    }
+    // if cb[i].checked: false -> rs = false
+    all.checked = rs;
+}
+
